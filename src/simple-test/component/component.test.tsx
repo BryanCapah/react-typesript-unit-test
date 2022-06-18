d73aff8766d6dacac195bf2f3a05bf9e24ae5aeb
brian capah <briansabbath@klique.id>
2022-06-18 20:59:45 +0700

    remove unused method

--- src/simple-test/component/component.test.tsx
+++ src/simple-test/component/component.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent } from "@testing-library/react"
-import userEvent from '@testing-library/user-event'
 import { TestPlayround, Title } from "../../component/title"
 
 test('cek teks title jika ada props name', async () => {
